b65c60cb8e205d97c9a8c1cff5ff69b5e3dbe099
shubham88fru <shubham88fru@yahoo.com>
2024-11-09 15:27:20 -0700

    feat: inflation adjustment

--- first-project/investment_calculator.go
+++ first-project/investment_calculator.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	const inflationRate = 6.5
 	var investmentAmount float64 = 1000
 	var expectedReturnRate float64 = 5.5
 	var years float64 = 10
@@ -16,5 +17,8 @@ func main() {
 	//go with a usual assignment using `var`.
 	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years)
 
+	futureRealValue := futureValue/math.Pow(1 + inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
