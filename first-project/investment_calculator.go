c90d2ced024c757298286d04955e7da92182bff5
shubham88fru <shubham88fru@yahoo.com>
2024-11-09 15:22:33 -0700

    feat: assignment shorthand notation

--- first-project/investment_calculator.go
+++ first-project/investment_calculator.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
-	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate/100), float64(years))
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 10
+
+	//Shortcut that can be used to declare and assign a varible, where
+	//the type is okay to be infered by go. NOTE: We can leverage this syntax
+	//when we explicitly provide type for a variable, in such case we'll have to
+	//go with a usual assignment using `var`.
+	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years)
 
 	fmt.Println(futureValue)
 }
