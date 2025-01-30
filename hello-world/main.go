f4e219cc087fc9b297551ce18f233264ddacd22f
shubham88fru <shubham88fru@yahoo.com>
2025-01-29 18:40:35 -0700

    render a template

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
-	"strconv"
 )
 
 const port = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	// w.Write([]byte("Hello, World!"))
+	renderTemplate(w, "home.page")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum, _ := addValues(2, 3)
-	w.Write([]byte("About Page" + strconv.Itoa(sum)))
+	// sum, _ := addValues(2, 3)
+	// w.Write([]byte("About Page" + strconv.Itoa(sum)))
+	renderTemplate(w, "about.page")
+
 }
 
 func addValues(x, y int) (int, error) {
 	return x + y, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl + ".html")
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Print("Error parsing template: ", err)
+	}
 }
 
 func main() {
