58e29ef368558979d13a1dd3a4b71d9af0d9f417
shubham88fru <shubham88fru@yahoo.com>
2025-02-02 18:22:07 -0700

    base template

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 )
 
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl + ".html")
-	err := parsedTemplate.Execute(w, nil)
+	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl + ".html", "templates/base.layout.html")
+	err := parsedTemplate.ExecuteTemplate(w, tmpl+".html", nil)
 
 	if err != nil {
 		fmt.Print("Error parsing template: ", err)
