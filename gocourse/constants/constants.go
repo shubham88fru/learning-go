889849229a73eac54378e2cd1ad785ec7f27bbcc
shubham88fru <shubham88fru@yahoo.com>
2025-05-20 15:52:49 -0700

    consts

---
+++ gocourse/constants/constants.go
@@ -0,0 +1,18 @@
+package main
+
+const pi = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const days int = 7
+
+	const (
+		Monday    = 1
+		Tuesday   = 2
+		Wednesday = 3
+		Thursday  = 4
+		Friday    = 5
+		Saturday  = 6
+		Sunday    = 7
+	)
+}
